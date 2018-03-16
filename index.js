085af084f84350b6630370feb7de9e6742707291
Pasi Hirvonen <pasi.hirvonen@octo3.fi>
2018-03-16 18:54:44 +0200

    Redis

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+console.log("REDIS HOST", process.env.REDIS_URL);
+
+const redis = require("redis"),
+  client = redis.createClient(process.env.REDIS_URL);
+
+client.on("error", function(err) {
+  console.log("Error " + err);
+});
+
+client.set("string key", "string val", redis.print);
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
